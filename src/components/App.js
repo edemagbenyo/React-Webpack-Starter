913e9c1f0d297940157f4e2b8af1232c0e47814c
Edemone <edem.agbenyo@gmail.com>
2019-11-10 19:40:19 +0000

    Initial commit & app working

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+    render() {
+        return (
+            <div>
+                <h2>Hey i am a react app built from scratch from vim</h2>
+            </div>
+        );
+    }
+}
+
+export default App;
